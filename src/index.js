8ae9b77629153d94144e0d6775e2d448121e58e0
melodyzhao <melodyzhao@lexin.com>
2020-11-20 20:15:32 +0800

    add records

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 const Koa = require('koa')
 const PORT = process.env.PORT || 5000;
-
 const app = new Koa()
+
+// logger
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Respence-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`)
+})
+
+// x-response-time
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Respence-Time', `${ms}ms`)
+})
 
 app.use(ctx => {
     ctx.body = `
